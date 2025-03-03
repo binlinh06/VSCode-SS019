48218fc33641456e8662f82828f8898fa0c95d3a
binlinh06 <nglinh150606@gmail.com>
2025-03-03 15:51:58 +0700

    Update SS19_LT07.js

--- Bai07/SS19_LT07.js
+++ Bai07/SS19_LT07.js
@@ -1,5 +1,8 @@
-let numA = +prompt("Mời bạn nhập số A");
-let numB = +prompt("Mời bạn nhập số B");
-let numC = +prompt("Mời bạn nhập số C");
-let Max=Math.max(numA,numB,numC);
-alert("Số lớn nhất là: " + Max)
+let numA = +prompt("Mời bạn nhập số a")
+let numB = +prompt("Mời bạn nhập số b")
+let numC = +prompt("Mời bạn nhập số c")
+let max = (numA > numB)
+    ? (numA > numC ? numA : numC)
+    : (numB > numC ? numB : numC)
+
+alert(`Số lớn nhất trong 3 số là: ${max}`)
